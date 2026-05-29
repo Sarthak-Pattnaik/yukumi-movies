e58c010595d3cdeba20171706cb48de3dc4a5223
Sarthak Pattnaik <psarthak@protonmail.com>
2026-05-29 20:54:05 +0530

    removed localhost links for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,11 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(cookieParser());
